b1ae1e86cdd28f4d53a11d90ff9fc38ba6073ebd
AdityaChourey <aditya77316@gmail.com>
2021-10-21 10:45:30 +0530

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Info from "./Info";
+import Repos from "./Repos";
+import User from "./User";
+import Search from "./Search";
+import Navbar from "./Navbar";
+
+//domain = dev-8kumpkpc.us.auth0.com
+// client ID = qgtN123DYUXytQlITkaeZ3h1RXTXuYYq
+
+export { Info, Repos, User, Search, Navbar };
